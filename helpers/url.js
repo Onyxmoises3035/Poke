43e65ce1dd58c7f564000e6d355aab336c22ef13
Onyxmoises3035 <maumoy1229@gmail.com>
2024-10-24 13:05:52 -0600

    clean

--- helpers/url.js
+++
@@ -1,6 +0,0 @@
-export const allPokemon = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
-
-export const onePokemon = (poke) => {
-    const pokemon = `https://pokeapi.co/api/v2/pokemon/${poke}`
-    return pokemon;
-}
