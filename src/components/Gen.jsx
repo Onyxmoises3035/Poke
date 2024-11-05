523d5354af297b4dfeb8eaf2dca0a3a2fec432f4
Onyxmoises3035 <maumoy1229@gmail.com>
2024-11-04 18:33:49 -0600

    render min

--- src/components/Gen.jsx
+++ src/components/Gen.jsx
@@ -18,26 +18,71 @@ const Gen = ({ gen }) => {
     ]
 
     const url = 'https://pokeapi.co/api/v2/pokemon/';
+    const lim = limGen[gen - 1].lim;
     const [pokemon, setPokemon] = useState([])
+    const [pokeLim1, setPokeLim1] = useState()
+    const [pokeLim2, setPokeLim2] = useState()
+    const [pokeStop, setPokeStop] = useState(true)
+    const [cont, setCont] = useState(0)
 
     useEffect(() => {
-        const lim = limGen[gen - 1].lim;
         const data = async () => {
             let pokemon = [];
-            for (let i = lim[0]; i <= lim[1]; i++) {
+            for (let i = lim[0]; i <= (lim[0] + 19); i++) {
                 const data = await axios.get(url + i).then(res => { return (res.data) });
                 pokemon.push(data)
             }
             return pokemon;
         }
         data().then(res => setPokemon(res));
+        setPokeLim1(lim[0] + 20)
+        setPokeLim2(lim[1])
+        setCont(0)
+        setPokeStop(true)
     }, [gen])
 
+    useEffect(() => {
+        if (pokeStop) {
+            if (cont != 0) {
+                if ((pokeLim1 + 20) > pokeLim2) {
+                    const data = async () => {
+                        let pokemon = [];
+                        for (let i = pokeLim1; i <= pokeLim2; i++) {
+                            const data = await axios.get(url + i).then(res => { return (res.data) });
+                            pokemon.push(data)
+                        }
+                        return pokemon;
+                    }
+                    data().then(res => setPokemon(res));
+                    setPokeStop(false)
+                } else {
+                    const data = async () => {
+                        let pokemon = [];
+                        for (let i = pokeLim1; i <= (pokeLim1 + 19); i++) {
+                            const data = await axios.get(url + i).then(res => { return (res.data) });
+                            pokemon.push(data)
+                        }
+                        return pokemon;
+                    }
+                    data().then(res => setPokemon(res));
+                }
+                setPokeLim1(pokeLim1 + 20)
+            }
+        }
+    }, [cont])
+
+    const morePokemon = () => {
+        setCont(cont + 1)
+    }
+
     return (
-        <div id="container" className="grid grid-cols-1 lg:grid-cols-5 mt-10">
-            {pokemon.map(poke => (
-                <Card key={poke.id} poke={poke} />
-            ))}
+        <div className="flex">
+            <div id="container" className="grid grid-cols-1 lg:grid-cols-5 mt-10">
+                {pokemon.map(poke => (
+                    <Card key={poke.id} poke={poke} />
+                ))}
+            </div>
+            {pokeStop ? <button className="bg-black bg-opacity-30 p-3 rounded-lg text-center" onClick={morePokemon}>mas</button> : <></>}
         </div>
     )
 }
